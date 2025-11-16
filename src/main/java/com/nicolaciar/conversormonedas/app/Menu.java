15745a2100ad4ee62bc1bb61196f28fd5e68cef5
nicolaciar <nicolas.laciar@mi.unc.edu.ar>
2025-11-16 11:24:55 -0300

    Agrega Menu

---
+++ src/main/java/com/nicolaciar/conversormonedas/app/Menu.java
@@ -0,0 +1,20 @@
+package com.nicolaciar.conversormonedas.app;
+
+public class Menu {
+    private final String contenido = """
+            *************************************);
+            Sea bienvenido/a al Conversor de Moneda =]
+            
+            1) Dólar ==> Peso argentino
+            2) Peso argentino ==> Dólar
+            3) Dólar ==> Real brasileño
+            4) Real brasileño ==> Dólar
+            5) Dólar ==> Peso colombiano
+            6) Peso colombiano ==> Dólar
+            7) Salir
+            Elija una opción válida: """;
+
+    public void mostrar(){
+        System.out.print(contenido);
+    }
+}
